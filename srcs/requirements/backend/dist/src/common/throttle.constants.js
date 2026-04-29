9cde895b27ceef04d466f1acefce80994a7df7a0
Axel Baudrillart <axbaudri@f2r3s12.clusters.42paris.fr>
2026-04-29 15:41:51 +0200

    updated frontend

--- srcs/requirements/backend/dist/src/common/throttle.constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.THROTTLE_LIMIT_API = exports.THROTTLE_LIMIT_AUTH = void 0;
-exports.THROTTLE_LIMIT_AUTH = { ttl: 60000, limit: 5 };
-exports.THROTTLE_LIMIT_API = { ttl: 60000, limit: 60 };
-//# sourceMappingURL=throttle.constants.js.map
--- srcs/requirements/frontend/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './style.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <h1>Hello World</h1>
-)
